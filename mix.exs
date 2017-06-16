331f6c8a543435d07d5b9ebabd86bc2a9ff06131
Yury Gargay <yury.gargay@gmail.com>
2017-06-16 17:57:26 +0200

    Prepare to publush on hex.pm

--- mix.exs
+++ mix.exs
@@ -7,7 +7,11 @@ defmodule MixDebeam.Mixfile do
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     description: description(),
+     package: package(),
+     deps: deps(),
+     name: "MIX Debeam",
+     source_url: "https://github.com/surik/mix_debeam"]
   end
 
   def application do
@@ -16,5 +20,16 @@ defmodule MixDebeam.Mixfile do
 
   defp deps do
     []
+  end
+
+  defp description do
+    "MIX task to get Erlang code of Elixir module."
+  end
+
+  defp package do
+    [name: :mix_debeam,
+     maintainers: ["Yury Gargay"],
+     licenses: ["Apache 2.0"],
+     links: %{"Github" => "https://github.com/surik/mix_debeam"}]
   end
 end
