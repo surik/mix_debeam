62094982f6b727191f866041e9607dce4585db40
Yury Gargay <yury.gargay@gmail.com>
2017-06-17 21:40:39 +0200

    Don't need Elixir prefix in module name
    Now, both
    
        $ mix debeam Elixir.Some.Module
    
    and
    
        $ mix debeam Some.Module
    
    are the same.

--- lib/mix/tasks/debeam.ex
+++ lib/mix/tasks/debeam.ex
@@ -1,25 +1,46 @@
 defmodule Mix.Tasks.Debeam do
   use Mix.Task
 
+  @shortdoc "Get Erlang code of built Elixir module"
+
+  @moduledoc """
+  Get Erlang code of built Elixir module.
+
+      mix debeam MODULE
+
+  ## Usage
+
+      mix debeam Some.Module > some_module.erl
+
+  Yoy can open `some_module.erl` by any text editor to see Erlang code of your Elixir module.
+
+  """
+
   alias :beam_lib, as: BeamLib
   alias :erl_syntax, as: ErlSyntax
   alias :erl_prettypr, as: ErlPrettyPr
 
-  @shortdoc "Get Erlang code of built Elixir module"
-
-  def run([module]) do
-    file = Path.join([Mix.Project.app_path, "ebin", module]) <> ".beam"
+  def run(["Elixir." <> _ = module]) do
+    file = Path.join([Mix.Project.app_path(), "ebin", module]) <> ".beam" 
     if File.exists?(file) do
-      {:ok, {_, [{:abstract_code, {_, ast}}]}} = 
-        to_charlist(file)
-        |> BeamLib.chunks([:abstract_code])
-      ErlSyntax.form_list(ast)
-      |> ErlPrettyPr.format()
-      |> IO.puts
-      true
+      debeam(file)
     else
-      IO.puts "Can find #{module}"
+      IO.puts "Can not find #{module}."
       false
+    end
+  end
+  def run([module]), do: run(["Elixir." <> module])
+
+  defp debeam(file) do
+    case BeamLib.chunks(to_charlist(file), [:abstract_code]) do
+      {:ok, {_, [{:abstract_code, {_, ast}}]}} ->
+        ErlSyntax.form_list(ast)
+        |> ErlPrettyPr.format()
+        |> IO.puts
+        true
+      _ ->
+        IO.puts "#{file} is not a beam file."
+        false
     end
   end
 end
--- test/debeam_test.exs
+++ test/debeam_test.exs
@@ -9,6 +9,12 @@ defmodule DebeamTest do
     end)
   end
 
+  test "run for available module without Elixir prefix" do
+    assert capture_io(fn ->
+      assert Mix.Tasks.Debeam.run(["Mix.Tasks.Debeam"])
+    end)
+  end
+
   test "run for non available module" do
     assert capture_io(fn ->
       assert not Mix.Tasks.Debeam.run(["Elixir.Unknown.Module"])
